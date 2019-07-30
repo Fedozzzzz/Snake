b27b16603ebaf7b902f46ee84f2b86aa5803adf9
FedozZz <titov.bigf@gmail.com>
2019-07-30 23:59:33 +0300

    refactored code

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
+import time
 from control import Control
 from snake import Snake
 from gui import Gui
 from food import Food
 
 pygame.init()
-
 window = pygame.display.set_mode((441, 441))
 pygame.display.set_caption("Snake")
 control = Control()
@@ -14,7 +14,6 @@ food = Food()
 gui = Gui()
 gui.init_field()
 food.generate_food_position(gui)
-speed = 0
 
 while control.flag_game:
     gui.check_win_or_lose()
@@ -23,19 +22,18 @@ while control.flag_game:
     if gui.game == "GAME":
         snake.draw_snake(window)
         food.draw_food(window)
+        if not control.flag_pause:
+            snake.move(control)
+            snake.check_barrier(gui)
+            snake.eat(food, gui)
+            snake.check_end_window()
+            snake.animation()
+        gui.draw_progress(window)
+        gui.draw_level(window)
     elif gui.game == "WIN":
         gui.draw_win(window)
     elif gui.game == "LOSE":
         gui.draw_lose(window)
-    gui.draw_progress(window)
-    gui.draw_level(window)
-    if speed % 60 == 0 and not control.flag_pause and gui.game == "GAME":
-        snake.move(control)
-        snake.check_barrier(gui)
-        snake.eat(food, gui)
-        snake.check_end_window()
-        snake.animation()
-    speed += 1
     pygame.display.flip()
-
+    time.sleep(0.1)
 pygame.quit()
